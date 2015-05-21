c374af98becd49ac15f37c556343e82358427a2a
Tobias Kortkamp <tobias.kortkamp@gmail.com>
2015-05-21 18:15:24 +0200

    Also set flycheck clang args in .dir-locals.el for syntax checking in Emacs

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,9 +4,10 @@
 ((nil
   (eval
    (lambda ()
-     (setq-local company-clang-arguments
-                 (progn
+     (let ((args (progn
                    (shell-cd (projectile-project-root))
                    (split-string (shell-command-to-string
                                   "make company-clang-arguments")
-                                 "\n")))))))
+                                 "\n"))))
+       (setq-local company-clang-arguments args)
+       (setq-local flycheck-clang-args args))))))
